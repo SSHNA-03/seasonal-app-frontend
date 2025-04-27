b6cf7d2ed29cf48f3c1e6726e739916cc29213c8
Midhun <midhunnagasaicse@gmail.com>
2025-04-27 22:57:04 +0530

    Colors Edited APP

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className="text-center danger ">
         <h1 className="text-2xl text-green-500 underline">Seasonal Products Application</h1>
         <div className="flex flex-col items-center justify-center h-40">
-          <h4 className="text-yellow-300 bg-red-700">Midhun Naga Sai</h4>
+          <h4 className="text-yellow-300 ">Midhun Naga Sai</h4>
           <h4 className="text-yellow-300 bg-red-700">Harsha Sree</h4>
         </div>
         <Button disabled>
