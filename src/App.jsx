9008b292359c90edcbfcb62f3922a66bd77f976f
Midhun <midhunnagasaicse@gmail.com>
2025-04-27 22:20:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <div className="text-center danger ">
+        <h1 className="text-2xl text-green-500 underline">Seasonal Products Application</h1>
+        <div className="flex flex-col items-center justify-center h-40">
+          <h4>Midhun Naga Sai</h4>
+          <h4>Harsha Sree</h4>
+        </div>
+        <Button disabled>
+          <Loader2 className="animate-spin" />
+          Started Building on 27-04-2025
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
